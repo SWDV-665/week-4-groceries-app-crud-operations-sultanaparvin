a26afe653785166f6107f158fc46648354ab9591
Sultana Parvin <Simy4825@gmail.com>
2018-11-13 20:47:10 -0500

    1.Added edit functionality 2. Created Alert service provider. 3.Created Grocery service provider. 4.Added message for no item in list and styled it using Sass

---
+++ groceries/src/pages/home/home.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  title = "Grocery";
+
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService : InputDialogServiceProvider) {
+
+  }
+
+  loadItems(){
+    return this.dataService.getItems();
+  }
+
+  editItem(item, index){
+    const toast = this.toastCtrl.create({
+      message : "Editing Item - "+item.name+" ...",
+      duration : 3000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item,index);
+  }
+
+  removeItem(item, index){
+    const toast = this.toastCtrl.create({
+      message : "Removing Item - "+item.name+" ...",
+      duration : 3000
+    });
+    toast.present();
+    this.dataService.removeItem(index);
+  }
+
+  addItem(){
+    this.inputDialogService.showPrompt();
+  }
+
+}
